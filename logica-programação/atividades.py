a760f6cb87b39c0a66f5489efbb7740ae2a41abd
vivianeborges353-commits <vivianeborges353@gmail.com>
2026-06-09 19:19:34 -0300

    Primeiro commit

---
+++ logica-programação/atividades.py
@@ -0,0 +1,36 @@
+#atividade 03
+# numeros=[8,5,3,4,5]
+# soma = 0
+
+# for i in range(5):
+#     numero = int(input("digite um numero:"))
+#     numeros.append(numero)
+
+# for numero in numeros:
+#     soma += numero
+# # print("soma total:",soma)
+
+#atividade 04
+# notas =[]
+# soma = 0
+
+# for i in range(4):
+#     nota = float(input("digite a nota:"))
+#     notas.append(nota)
+
+# for nota in notas:
+#     soma = soma + nota
+# media = soma /len(notas)
+
+# if media >=7:
+#     print("aprovado")
+# else: 
+#     print("reprovado")
+
+    #atividade 07
+# numeros=[10,5,8,20,3,15]
+
+# print("quantidade", len(numeros))
+# print("quantidade", max(numeros))
+# print("quantidade", min(numeros))
+
---
+++ poo.py
@@ -0,0 +1,57 @@
+# #SEM POO - Programação Orientada a Objeto
+# cor = "preto"
+# modelo ="civic"
+
+# def acelerar():
+#     print("O carro acelerou")
+
+# print(cor)
+# print(modelo)
+# acelerar()
+
+##Com Programação Orientada ao Objeto
+# class carro:
+#     def __init__(self, cor,modelo):
+#      self.cor = cor
+#      self.modelo = modelo
+
+#      def acelerar(self):
+#         print("o carro acelerou")
+
+# carro1=carro("preto", "civic")
+# carro2=carro("vermelho", "gol")
+
+# print(carro1.cor)
+# print(carro1.modelo)
+# print(carro2.cor)
+# print(carro2.modelo)
+# carro1.acelerar ()
+
+# class Conta:
+
+# def __init__(self, titular, saldo):
+#      self.titular = titular
+#      self.saldo = saldo
+
+# def depositar(self, valor):
+#     self.saldo += valor
+#     print("Depósito realizado")
+
+# def sacar(self, valor):
+#     self.saldo -= valor
+#     print("Saque realizado")
+
+# def mostrar_saldo(self):
+#     print(f"Saldo atual: {self.saldo}")
+
+# conta1 = Conta(“Maria", 1000)
+
+# print(conta1.titular)
+
+# conta1.depositar(500)
+# conta1.sacar(200)
+# conta1.mostrar_saldo()
+    
+#atividade02
+class Produtp:
+    def__init(self,nome,preço)
